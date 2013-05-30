99940f9e4ada00e1bbd6e728202ce99a071f2208
akm0012 <akm0012@auburn.edu>
2013-05-30 12:51:40 -0500

    Initial Commit.

---
+++ SideNavigationDemo/gen/edu/auburn/akm0012/sidenavigationdemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.auburn.akm0012.sidenavigationdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
